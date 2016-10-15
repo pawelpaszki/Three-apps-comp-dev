0a3b627603c60389995277f81fc9171b3ca50e1b
pawelpaszki <pawelpaszki@gmail.com>
2016-10-15 18:47:12 +0100

    contact list - static version - before rendering data set

--- contactList/src/App.js
+++ contactList/src/App.js
@@ -1,21 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+var ContactForm = React.createClass({
+  render: function(){
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <tr>
+      <td>
+      <input type="text" className="form-control" />
+      </td>
+      <td>
+      <input type="text" className="form-control"/>
+      </td>
+      <td>
+      <input type="text" className="form-control" />
+      </td>
+      <td>
+      <input type="button" className="btn btn-primary" value="Add"/>
+      </td>
+      </tr>
+    )
+  }
+});
+
+var Contact = React.createClass({
+  render: function(){
+    return (
+      <tr >
+      {/* TODO */}
+      </tr>
+
+    ) ;
+  }
+});
+
+var ContactList = React.createClass({
+  render: function(){
+    var contactRows = null ;  // TODO
+    return (
+      <tbody >
+      {contactRows}
+      <ContactForm />
+      </tbody>
+    ) ;
+  }
+});
+
+var ContactsTable = React.createClass({
+  render: function(){
+    return (
+      <table className="table table-bordered">
+      <thead>
+      <tr>
+      <th>Name</th>
+    <th>Address</th>
+    <th>Phone Number</th>
+    <th></th>
+    <th></th>
+    </tr>
+    </thead>
+    <ContactList contacts={this.props.contacts}  />
+    </table>
     );
   }
-}
+});
 
-export default App;
+var ContactsApp = React.createClass({
+  render: function(){
+    return (
+      <div>
+      <h1>Contact List.</h1>
+    <ContactsTable contacts={this.props.contacts}  />
+    </div>
+    );
+  }
+});
+
+export default ContactsApp;
--- contactList/src/index.js
+++ contactList/src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import ContactsApp from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+var contacts = [
+  {
+    "name": "Contact 1",
+    "address": "123 Test St",
+    "phone_number": "132-3212"
+  },
+
+  {
+    "name": "Contact 2",
+    "address": "23 Main St",
+    "phone_number": "934-4329"
+  },
+
+  {
+    "name": "Contact 3",
+    "address": "4 Lower St",
+    "phone_number": "432-5832"
+  },
+
+  {
+    "name": "Contact 4",
+    "address": "49 Upper Street",
+    "phone_number": "934-4290"
+  }
+] ;
 
 ReactDOM.render(
-  <App />,
+<ContactsApp  contacts={contacts}  />,
   document.getElementById('root')
 );
